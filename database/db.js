7b69a7d60547215ff7d776b050e80f1907403078
MatthewGimlewicz <gimlewiczm@gmail.com>
2019-11-13 18:09:56 -0500

    Updated database example

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 
-var connection = "postgres://pcbhbjxr:AiaTJ3r7BSJJLfx-bfauVsrImRA1X6u8@salt.db.elephantsql.com:5432/pcbhbjxr";
+var connection = "postgres://oohprrpx:VCPIDRy5mbc2oRw7fiPqVEwDgs5GfW8R@salt.db.elephantsql.com:5432/oohprrpx";
 
 var client = new pg.Client(connection);
 
@@ -9,11 +9,18 @@ client.connect(function(err) {
         console.log(err);
     } 
     console.log("Connection successful... querying\n");
-    client.query("SELECT * FROM STUDENTS", function(err, result) {
+    client.query("SELECT * FROM Course", function(err, result) {
         if (err) {
             return console.error("error running query", err);
         }
-        console.log(result.rows[0]);
+        console.log("-----------------------------------\n")
+        result.rows.forEach(function(row) {
+            console.log("Course number: " + row.cnum + "\n");
+            console.log("Course name: " + row.name + "\n");
+            console.log("Credits: " + row.credits + "\n");
+            console.log("-----------------------------------\n")
+        });
+        console.log(result.rows);
         client.end();
     });
 });
