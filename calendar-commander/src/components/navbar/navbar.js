4ed2a93b3464b1436e16f1a9ae1249e9802ffb78
Jacob Calkins <jcalkins@umass.edu>
2019-12-08 10:21:36 -0500

    added mediocre navbar component

--- calendar-commander/src/components/navbar/navbar.js
+++ calendar-commander/src/components/navbar/navbar.js
@@ -8,7 +8,17 @@ function App() {
     <div className="App">
       <div id="navbar"> <!-- TODO TURN INTO COMPONENT -->
         <h1>Calendar Commander</h1>
-        <img src="./calendar-commander/src/calendar_icon.png" />
+        <figure class="recolor"><img src="./calendar-commander/src/calendar_icon.png" /></figure>
+        <div class="menu">
+          <div class="menu" id="menu-item">
+            <figure class="recolor"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn0.iconfinder.com%2Fdata%2Ficons%2Ftypicons-2%2F24%2Farrow-back-512.png&f=1&nofb=1" /></figure>
+            <h3>Back to SPIRE</h3>
+          </div>
+          <div class="menu" id="menu-item">
+            <figure class="recolor"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.endlessicons.com%2Fwp-content%2Fuploads%2F2014%2F03%2Fbookmark-icon-3.png&f=1&nofb=1" /></figure>
+            <h3>Saved Schedules</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
