44d37cc5446a786aafbc2dc0d2c50ae6d7f941f9
MatthewGimlewicz <gimlewiczm@gmail.com>
2019-12-08 14:40:18 -0500

    Fixed DB query

--- server/generate.py
+++ server/generate.py
@@ -23,7 +23,9 @@ def connect():
             data = json.load(json_file)
             for i in data["required_courses"]["unsatisfied"]:
                 #print(i)
-                print(cur.execute("SELECT * courses where cnum = '%s' and major='%s'", i['cnums'], i['major']))
+                cur.execute("SELECT * FROM courses where cnum = %s and major=%s", ((i['cnum'], i['major'])))
+                results = cur.fetchall()[0]
+                print(results)
     
             
 
