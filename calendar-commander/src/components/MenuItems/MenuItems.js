9ef56efe9a05043c06fd17efcc186f0ff9f5e9bb
Jacob Calkins <jcalkins@umass.edu>
2019-12-08 15:37:13 -0500

    Added menu items and SUPER DUPER HIGH RESOLUTION ICON

--- calendar-commander/src/components/MenuItems/MenuItems.js
+++ calendar-commander/src/components/MenuItems/MenuItems.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './MenuItems.css';
+import addIcon from './addIcon.png';
+import backIcon from './backIcon.png';
+import shoppingCart from './shoppingCartIcon.png';
 
 class MenuItems extends React.Component {
   render() {
     return (
       <div className="MenuItems">
-        <h2>Enter your Student ID</h2>
+        <div class="options">
+            <button class="option-click">
+              <img class="option-icon" src={addIcon} alt="plus icon" />
+              <h3 class="option-title">Add to Saved Schedules</h3>
+            </button>
+            <button class="option-click">
+              <img  class="option-icon" src={backIcon} alt="back icon" />
+              <h3 class="option-title">Edit Requirements</h3>
+            </button>
+            <button class="option-click">
+              <img class="option-icon" src={shoppingCart} alt="shopping cart" />
+              <h3 class="option-title">Add to Shopping Cart</h3>
+            </button>
+        </div>
       </div>
     );
   }
