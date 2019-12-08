4fa2e54bc7fd4d388fcb73ff4513c040eb89a835
Jacob Calkins <jcalkins@umass.edu>
2019-12-08 14:28:03 -0500

    broked it

---
+++ calendar-commander/src/components/MenuItems/MenuItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MenuItems.css';
+
+class MenuItems extends React.Component {
+  render() {
+    return (
+      <div className="MenuItems">
+        <h2>Enter your Student ID</h2>
+      </div>
+    );
+  }
+}
+
+export default MenuItems;
--- calendar-commander/src/index.js
+++ calendar-commander/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import NavBar from "./components/navbar/NavBar"
-import StudentID from "./components/student_id_container/StudentID"
-import ProgressContainer from "./components/progress_container/ProgressContainer"
-import RequirementsContainer from "./components/requirements_container/RequirementContainers"
-import Schedule from "./components/schedule_generator/Schedule"
+import NavBar from "./components/navbar/NavBar";
+import StudentID from "./components/student_id_container/StudentID";
+import ProgressContainer from "./components/progress_container/ProgressContainer";
+import RequirementsContainer from "./components/requirements_container/RequirementContainers";
+import Schedule from "./components/schedule_generator/Schedule";
+import MenuItems from "./components/MenuItems/MenuItems";
 
-ReactDOM.render(<div><NavBar /> <Schedule /></div>, document.getElementById('root'));
+ReactDOM.render(<div><NavBar /><MenuItems /><Schedule /></div>, document.getElementById('root'));
