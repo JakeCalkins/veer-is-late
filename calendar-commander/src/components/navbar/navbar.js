c158af373f00ec29848be1dbd946f54f6fdc5e76
Peter Singh <digitalizedveer@gmail.com>
2019-12-08 12:47:27 -0500

    Developed Navigation Bar
    Repositioned, fixed sizing, added border, and inline blocks

--- calendar-commander/src/components/navbar/navbar.js
+++ calendar-commander/src/components/navbar/navbar.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import "./NavBar.css"
-import calendar_icon from './calendar_icon.png';
+import calendar_icon from './calendar.png';
+import back_logo from './backLogo.png';
+import saved_icon from './savedIcon.png';
 
 class NavBar extends React.Component {
  render() {
     return (
       <div className="NavBar">
         <div id="navbar">
-          <h1>Calendar Commander</h1>
-          <figure class="recolor"><img src={calendar_icon} /></figure>
           <div class="menu">
-            <div id="spire-return" style="float: left">
-              <figure class="recolor"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn0.iconfinder.com%2Fdata%2Ficons%2Ftypicons-2%2F24%2Farrow-back-512.png&f=1&nofb=1" /></figure>
+            <div id = "title-header">
+              <h1>Calendar Commander</h1>
+            </div>
+            <div class="img">
+              <img class = "calendar" src={calendar_icon} />
+            </div>
+            <div id="spire-return">
+              <img src={back_logo} alt="go back"/>
               <h3>Back to SPIRE</h3>
             </div>
             <div id="saved-schedules" >
-              <figure class="recolor"><img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.endlessicons.com%2Fwp-content%2Fuploads%2F2014%2F03%2Fbookmark-icon-3.png&f=1&nofb=1" /></figure>
+              <img src={saved_icon} alt="saved schedules"></img>
               <h3>Saved Schedules</h3>
             </div>
           </div>
--- calendar-commander/src/index.js
+++ calendar-commander/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import ProgressContainer from "./components/progress_container/ProgressContainer"
+import NavBar from "./components/navbar/NavBar"
 
-ReactDOM.render(<ProgressContainer />, document.getElementById('root'));
+ReactDOM.render(<NavBar />, document.getElementById('root'));
