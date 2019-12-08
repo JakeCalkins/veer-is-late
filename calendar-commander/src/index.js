ebf0d8c290b9e6c198054652be2b38c57bb9d79a
Jacob Calkins <jcalkins@umass.edu>
2019-12-08 12:11:21 -0500

    new white shiny calendar

--- calendar-commander/src/index.js
+++ calendar-commander/src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import StudentID from "./components/student_id_container/StudentID"
-import NavBar from "./components/navbar/NavBar"
 import ProgressContainer from "./components/progress_container/ProgressContainer"
 
-ReactDOM.render(<NavBar />, document.getElementById('root'));
-console.log(StudentID)
-
-// NOTE: removed service worker for the MVP
+ReactDOM.render(<ProgressContainer />, document.getElementById('root'));
