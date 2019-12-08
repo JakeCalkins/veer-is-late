37b5a8cfbcdd12ce8a4b2c99a229988a85c5e96b
Mark <dowmark4686@gmail.com>
2019-12-08 14:33:16 -0500

    initial schedule maker

---
+++ server/generate.py
@@ -0,0 +1,45 @@
+import psycopg2
+import json
+
+def connect():
+    """ Connect to the elephantSQL server """
+    conn = None
+    try:
+        # Connect to elephantSQL
+        print("\nConnecting to elephantSQL server\n")
+        conn = psycopg2.connect(host="salt.db.elephantsql.com", database="oohprrpx", user="oohprrpx", password="VCPIDRy5mbc2oRw7fiPqVEwDgs5GfW8R")
+
+        print("Connection successful\n")
+
+        # Successful connection, create cursor to parse query results
+        cur = conn.cursor()
+        print("Executing queries\n")
+
+        with open("server/courses/output.json") as json_file:
+            
+            schedule = []
+
+            # Load JSON data
+            data = json.load(json_file)
+            for i in data["required_courses"]["unsatisfied"]:
+                #print(i)
+                print(cur.execute("SELECT * courses where cnum = '%s' and major='%s'", i['cnums'], i['major']))
+    
+            
+
+
+        #cur.execute("INSERT INTO Coursestemp VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", ((cnum, title, "4", pid, lid, "0", "TBD", "TBD", major, "TBD", times, typ)))
+        #cur.execute("INSERT INTO Coursestemp VALUES(%s, %s, %s, %s, null, %s, %s, %s, %s, %s, %s, %s)", ((cnum, title, "3", pid, "0", "TBD", "TBD", major, "TBD", times, typ)))
+        #cur.execute("INSERT INTO Coursestemp VALUES(%s, %s, %s, null, null, %s, %s, %s, %s, %s, %s, %s)", ((cnum, title, "3", "0", "TBD", "TBD", major, "TBD", times, typ)))
+                
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if conn is not None:
+            conn.close()
+            print("\nDatabase connection closed\n")
+
+if __name__ == "__main__":
+    connect()
