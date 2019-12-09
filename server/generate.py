b30d4cc241c8cd1b104c0d55276dcf513d5eeb99
Mark <dowmark4686@gmail.com>
2019-12-08 23:17:56 -0500

    what in the kentucky fried bs :hankey:

--- server/generate.py
+++ server/generate.py
@@ -1,7 +1,121 @@
 import psycopg2
 import json
+import re
+import random
 
-def connect():
+def check_conflicts(schedule_arr, newclass):
+    for c in schedule_arr:
+        #see if classes are on the same day
+        conflict = False
+        if "M" in c[10] and "M" in newclass[10]:
+            #print("found")
+            time = re.compile("\d+")
+            ampm = re.compile("[A-Z]+")
+            fixedstartc = float(time.findall(c[10])[0]) + (float(time.findall(c[10])[1])/60.0)
+            if(ampm.findall(c[10])[-2] == "PM"):
+                fixedstartc += 12.0
+            fixedstart = float(time.findall(newclass[10])[0]) + (float(time.findall(newclass[10])[1])/60.0)
+            if(ampm.findall(newclass[10])[-2] == "PM"):
+                fixedstart += 12.0
+            fixedendc = float(time.findall(c[10])[2]) + (float(time.findall(c[10])[3])/60.0)
+            if(ampm.findall(c[10])[-1] == "PM"):
+                fixedendc += 12.0
+            fixedend = float(time.findall(newclass[10])[2]) + (float(time.findall(newclass[10])[3])/60.0)
+            if(ampm.findall(newclass[10])[-1] == "PM"):
+                fixedend += 12.0
+
+            if(max(fixedstart, fixedstartc) < min(fixedend, fixedendc)):
+                #print("Conflict")
+                conflict =  True
+
+        if "TU" in c[10] and "TU" in newclass[10]:
+            #print("found")
+            time = re.compile("\d+")
+            ampm = re.compile("[A-Z]+")
+            fixedstartc = float(time.findall(c[10])[0]) + (float(time.findall(c[10])[1])/60.0)
+            if(ampm.findall(c[10])[-2] == "PM"):
+                fixedstartc += 12.0
+            fixedstart = float(time.findall(newclass[10])[0]) + (float(time.findall(newclass[10])[1])/60.0)
+            if(ampm.findall(newclass[10])[-2] == "PM"):
+                fixedstart += 12.0
+            fixedendc = float(time.findall(c[10])[2]) + (float(time.findall(c[10])[3])/60.0)
+            if(ampm.findall(c[10])[-1] == "PM"):
+                fixedendc += 12.0
+            fixedend = float(time.findall(newclass[10])[2]) + (float(time.findall(newclass[10])[3])/60.0)
+            if(ampm.findall(newclass[10])[-1] == "PM"):
+                fixedend += 12.0
+
+            if(max(fixedstart, fixedstartc) < min(fixedend, fixedendc)):
+                #print("Conflict")
+                conflict =  True
+
+        if "W" in c[10] and "W" in newclass[10]:
+            #print("found")
+            time = re.compile("\d+")
+            ampm = re.compile("[A-Z]+")
+            fixedstartc = float(time.findall(c[10])[0]) + (float(time.findall(c[10])[1])/60.0)
+            if(ampm.findall(c[10])[-2] == "PM"):
+                fixedstartc += 12.0
+            fixedstart = float(time.findall(newclass[10])[0]) + (float(time.findall(newclass[10])[1])/60.0)
+            if(ampm.findall(newclass[10])[-2] == "PM"):
+                fixedstart += 12.0
+            fixedendc = float(time.findall(c[10])[2]) + (float(time.findall(c[10])[3])/60.0)
+            if(ampm.findall(c[10])[-1] == "PM"):
+                fixedendc += 12.0
+            fixedend = float(time.findall(newclass[10])[2]) + (float(time.findall(newclass[10])[3])/60.0)
+            if(ampm.findall(newclass[10])[-1] == "PM"):
+                fixedend += 12.0
+
+            if(max(fixedstart, fixedstartc) < min(fixedend, fixedendc)):
+                #print("Conflict")
+                conflict =  True
+
+        if "TH" in c[10] and "TH" in newclass[10]:
+            #print("found")
+            time = re.compile("\d+")
+            ampm = re.compile("[A-Z]+")
+            fixedstartc = float(time.findall(c[10])[0]) + (float(time.findall(c[10])[1])/60.0)
+            if(ampm.findall(c[10])[-2] == "PM"):
+                fixedstartc += 12.0
+            fixedstart = float(time.findall(newclass[10])[0]) + (float(time.findall(newclass[10])[1])/60.0)
+            if(ampm.findall(newclass[10])[-2] == "PM"):
+                fixedstart += 12.0
+            fixedendc = float(time.findall(c[10])[2]) + (float(time.findall(c[10])[3])/60.0)
+            if(ampm.findall(c[10])[-1] == "PM"):
+                fixedendc += 12.0
+            fixedend = float(time.findall(newclass[10])[2]) + (float(time.findall(newclass[10])[3])/60.0)
+            if(ampm.findall(newclass[10])[-1] == "PM"):
+                fixedend += 12.0
+
+            if(max(fixedstart, fixedstartc) < min(fixedend, fixedendc)):
+                #print("Conflict")
+                conflict =  True
+
+        if "F" in c[10] and "F" in newclass[10]:
+            #print("found")
+            time = re.compile("\d+")
+            ampm = re.compile("[A-Z]+")
+            fixedstartc = float(time.findall(c[10])[0]) + (float(time.findall(c[10])[1])/60.0)
+            if(ampm.findall(c[10])[-2] == "PM"):
+                fixedstartc += 12.0
+            fixedstart = float(time.findall(newclass[10])[0]) + (float(time.findall(newclass[10])[1])/60.0)
+            if(ampm.findall(newclass[10])[-2] == "PM"):
+                fixedstart += 12.0
+            fixedendc = float(time.findall(c[10])[2]) + (float(time.findall(c[10])[3])/60.0)
+            if(ampm.findall(c[10])[-1] == "PM"):
+                fixedendc += 12.0
+            fixedend = float(time.findall(newclass[10])[2]) + (float(time.findall(newclass[10])[3])/60.0)
+            if(ampm.findall(newclass[10])[-1] == "PM"):
+                fixedend += 12.0
+
+            #check if times overlap
+            if(max(fixedstart, fixedstartc) < min(fixedend, fixedendc)):
+                #print("Conflict")
+                conflict =  True
+            
+        return conflict
+
+def generate(arr_dir, credits_min, credits_max, num_schedules):
     """ Connect to the elephantSQL server """
     conn = None
     try:
@@ -15,23 +129,65 @@ def connect():
         cur = conn.cursor()
         print("Executing queries\n")
 
-        with open("server/courses/output.json") as json_file:
+        #schedule list
+        schedules = []
+        num_generated = 0
+
+        with open(arr_dir) as json_file:
             
-            schedule = []
+            schedule_req = []
+            credits_added = 0
 
             # Load JSON data
+            #add required classes 
             data = json.load(json_file)
             for i in data["required_courses"]["unsatisfied"]:
+                #get clas info from database
                 cur.execute("SELECT * FROM courses where cnum = %s and major=%s", ((i['cnum'], i['major'])))
-                results = cur.fetchall()[0]
-                print(results)
-    
+                i = cur.fetchall()[0]
+                #print(i)
+                #schedule_req.append(i)
+                if(check_conflicts(schedule_req, i)):
+                    print("Couldn't build schedule with required classes")
+                    return -1
+                else:
+                    if((credits_added + i[2]) > credits_max):
+                        print("Required classes have too many credits")
+                        return -1
+                    else:
+                        schedule_req.append(i)
+                        credits_added += i[2]
+
+            #add schedule if it already has required credits
+            if(credits_added > credits_min):
+                schedules.append(schedule_req)
+                num_generated += 1
+
+            already_added_courses = []
+
+            schedules_list = []
+
+            for i in range(num_schedules):
+                gened = random.choice(data["geneds"])
+                print(gened)
+                new_sched = schedule_req[:]
+                new_creds = credits_added
+                while(new_creds < credits_min):
+                    query_val = "%" + str(*gened) + "%"
+                    cur.execute("SELECT * FROM courses WHERE gened LIKE %s and cnum like %s ORDER BY cnum", (query_val,"1%"))
+                    results = cur.fetchall()
+                    if(len(results) > 0):
+                        newclass = random.choice(results)
+                        if((check_conflicts(new_sched, newclass) == False) and (newclass not in already_added_courses)):
+                            new_sched.append(newclass)
+                            new_creds += newclass[2]
+                            already_added_courses.append(newclass)
+                schedules_list.append(new_sched)
             
+            return schedules_list
 
 
-        #cur.execute("INSERT INTO Coursestemp VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", ((cnum, title, "4", pid, lid, "0", "TBD", "TBD", major, "TBD", times, typ)))
-        #cur.execute("INSERT INTO Coursestemp VALUES(%s, %s, %s, %s, null, %s, %s, %s, %s, %s, %s, %s)", ((cnum, title, "3", pid, "0", "TBD", "TBD", major, "TBD", times, typ)))
-        #cur.execute("INSERT INTO Coursestemp VALUES(%s, %s, %s, null, null, %s, %s, %s, %s, %s, %s, %s)", ((cnum, title, "3", "0", "TBD", "TBD", major, "TBD", times, typ)))
+
                 
 
     except (Exception, psycopg2.DatabaseError) as error:
@@ -43,4 +199,4 @@ def connect():
             print("\nDatabase connection closed\n")
 
 if __name__ == "__main__":
-    connect()
+    print(generate("server/courses/output.json", 14, 18, 3))
