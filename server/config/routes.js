ce2689a0cbb85c6450ecc8a392cfadb47b3213ab
MatthewGimlewicz <gimlewiczm@gmail.com>
2019-12-09 14:31:24 -0500

    Python route finally working

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,5 @@
 var courseHandler = require('../courses/courseHandler');
+var fs = require('fs')
 
 module.exports = function(app, express) {
 
@@ -7,9 +8,18 @@ module.exports = function(app, express) {
 
     // GET request for schedule processing
     app.get('/api/schedule', function(req, res) {
-        // Use python shell to run shedule builder script
-        var PythonShell = require('python-shell').PythonShell;
-        var pyshell = new PythonShell('/Users/MatthewGimlewicz/Documents/veer-is-late/server/generator.py');
+        console.log("Received GET request to run Python script\n");
+        const { spawn } = require('child_process');
+        const pyProg = spawn('python', ['./server/generate.py']);
+        pyProg.on('exit', function() {
+            console.log('FINISHED\n')
+            fs.readFile('generated_schedules.txt', 'utf8', function(err, data){
+                    // console.log(data); //OK
+                    data = JSON.parse(data);
+                    res.json(data);
+                    res.send();
+            });
+        });
     });
 
 };
