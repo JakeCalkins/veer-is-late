338197f645e123a289b00cfb57a6ece62d6e7b69
Jacob Calkins <jcalkins@umass.edu>
2019-12-10 01:56:42 -0500

    rework of schedules to use props properly

--- calendar-commander/src/components/schedule_generator/Schedule.js
+++ calendar-commander/src/components/schedule_generator/Schedule.js
@@ -1,30 +1,101 @@
 import React from 'react';
 import './Schedule.css';
-import Mercenaries from '../ScheduleMercenaries/ScheduleMercenaries'
+// import Mercenaries from '../ScheduleMercenaries/ScheduleMercenaries'
 // import ScheduleMercenaries from './components/ScheduleMercenaries/ScheduleMercenaries';
 import DetailsPane from "../DetailsPane/DetailsPane"
 import arrow from "./detailSelector.png"
+import online from "./online.png"
+import study from "./study.png"
+import seminar from "./seminar.png"
+import schedulesJSON from '../../../../generated_schedules.json';
 
 class Schedule extends React.Component {
   render() {
+    let mercList = [];
+    for(scheduleNumber in schedulesJSON) {
+      mercList.push(<Mercenaries scheduleNumber={scheduleNumber} />);
+    }
     return (
       <div className="Schedule">
         <div class="schedule-wrappist" id="schedule-container">
-          <h2 class="schedule-header">Generated Schedules (X results)</h2>
+          <h2 class="schedule-header">Generated Schedules ({schedulesJSON.length} results)</h2>
           <div class="schedule-list">
-            <Mercenaries></Mercenaries>
-            <Mercenaries></Mercenaries>
-            <Mercenaries></Mercenaries>
-            <Mercenaries></Mercenaries>
+            { mercList }
           </div>
           <div class="calender-contain">
-            <DetailsPane></DetailsPane>
+            <DetailsPane />
           </div>
-          
         </div>
       </div>
     );
   }
+}
+
+function Mercenaries(props) {
+  const data = schedulesJSON[props.scheduleNumber];
+  const listItems = data.map((c) => <li>{c.cname}</li>);
+  for(item in listItems) {
+    if(item < 3) {
+        classListColumn1.push(listItems[item]);
+    } else if(item < 6 && item >= 3) {
+        classListColumn2.push(listItems[item]);
+    } else if(item < 9 && item >= 6) {
+        classListColumn2.push(listItems[item]);
+    } else {}
+  }
+
+  let credits = 0;
+  let numClasses= 0;
+  let inMajor = 0;
+  let outMajor = 0;
+
+  //totals
+  for(classes in data) {
+    credits += data[classes].credits;
+    classes ++;
+    if(data[classes].major === "COMPSCI") {
+      credits += data[classes].credits;
+    } else {
+      credits += data[classes].credits;
+    }
+  }
+
+  return (
+    <div className = "Mercenaries">
+        <div class="course-details" id="course-details-container">
+          <div class="course-header">
+              <h1 class="credit-count">{credits} credits</h1>
+              <h1 class="class-count">
+              <span class="num">{numClasses}</span> classes</h1>
+              <h1 class="major-count">
+              <span class="num">{inMajor}</span> in-major</h1>
+              <h1 class="other-count">
+                  <span class="num">{outMajor}</span> non-major</h1>
+          </div>
+          <hr class="breaker"/>
+          <div class="course-body">
+              <div class="course-icons">
+                  <img src={online}></img>
+                  <img src={study}></img>
+                  <img src={seminar}></img>
+              </div>
+              <div class="vl">
+              </div>
+              <div class="course-classes">
+                  <ul class="col col1">
+                      { classListColumn1 }
+                  </ul>
+                  <ul class="col col2">
+                      { classListColumn2 }
+                  </ul>
+                  <ul class="col col3">
+                      { classListColumn3 }
+                  </ul>
+                  </div>
+          </div>
+        </div>
+    </div>
+  );
 }
 
 export default Schedule;
--- calendar-commander/src/index.js
+++ calendar-commander/src/index.js
@@ -3,38 +3,38 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import NavBar from "./components/navbar/NavBar";
-import StudentID from "./components/student_id_container/StudentID";
+// import StudentID from "./components/student_id_container/StudentID";
 import ProgressContainer from "./components/progress_container/ProgressContainer";
-import RequirementsContainer from "./components/requirements_container/RequirementContainers";
+// import RequirementsContainer from "./components/requirements_container/RequirementContainers";
 import Schedule from "./components/schedule_generator/Schedule";
 import MenuItems from "./components/MenuItems/MenuItems";
-import Mercenaries from './components/ScheduleMercenaries/ScheduleMercenaries';
-import DetailsPane from './components/DetailsPane/DetailsPane';
+// import Mercenaries from './components/ScheduleMercenaries/ScheduleMercenaries';
+// import DetailsPane from './components/DetailsPane/DetailsPane';
 
-// ReactDOM.render(<div><NavBar /> <MenuItems/><Schedule /></div>, document.getElementById('root'));
-ReactDOM.render(<div><NavBar /> <StudentID/></div>, document.getElementById('root'));
+ReactDOM.render(<div><NavBar /> <MenuItems/><Schedule /></div>, document.getElementById('root'));
+// ReactDOM.render(<div><NavBar /> <StudentID/></div>, document.getElementById('root'));
 
-let updateView = function() {
-    x++;
-    if (x == 3) {
-        visibleView = (
-            <div>
-                <NavBar />
-                <RequirementsContainer />
-            </div>
-        );
-    }
-    if (x == 4) {
-        visibleView = (
-            <div>
-                <NavBar />
-                <MenuItems />
-                <Schedule />
-            </div>
-        );
-    }
-    // Render to the DOM
-ReactDOM.render(visibleView, document.getElementById('root'));
-}
+// let updateView = function() {
+//     x++;
+//     if (x == 3) {
+//         visibleView = (
+//             <div>
+//                 <NavBar />
+//                 <RequirementsContainer />
+//             </div>
+//         );
+//     }
+//     if (x == 4) {
+//         visibleView = (
+//             <div>
+//                 <NavBar />
+//                 <MenuItems />
+//                 <Schedule />
+//             </div>
+//         );
+//     }
+//     // Render to the DOM
+// ReactDOM.render(visibleView, document.getElementById('root'));
+// }
 
-export default updateView;
+// export default updateView;
